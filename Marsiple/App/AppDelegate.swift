2ddb9d3234d65f45bb0876bbb05d484acb107d1f
Matej Korman <matejkorman@matejs-mbp>
2018-07-20 11:29:08 +0200

    Created 'AlbumsView' populated with dummy pictures.

--- Marsiple/App/AppDelegate.swift
+++ Marsiple/App/AppDelegate.swift
@@ -10,10 +10,12 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]? = nil) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = TabBarController()
+        window?.makeKeyAndVisible()
         return true
     }
 }
---
+++ Marsiple/App/Tab Bar/TabBarController.swift
@@ -0,0 +1,22 @@
+//
+//  TabBarController.swift
+//  Marsiple
+//
+//  Created by Matej Korman on 18/07/2018.
+//  Copyright © 2018 Matej Korman. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+    init() {
+        super.init(nibName: nil, bundle: nil)
+        let navigationController = UINavigationController(rootViewController: AlbumsViewController())
+        navigationController.navigationBar.prefersLargeTitles = true
+        viewControllers = [navigationController]
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
