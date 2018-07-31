ab6921c9096dfc59f450cc8ffba48fef8488f36a
matej494 <matej494@gmail.com>
2018-07-31 10:15:22 +0200

    Post details - main (#11)
    * Post details - #1 View (#6)
    
    * Added 'PostDetailsView' and populated cells with dummy data.. Added 'TitleAndBodyView' as reusable 'view'.
    
    * Removed repeating line.
    
    * Overridden `hidesBottomBarWhenPushed` variable. Code refactoring.
    
    * Removed overriding of `hidesBottomBarWhenPushed` and set it's value to true.
    
    * Post details - #2 Fetching data (#7)
    
    * Added 'PostDetailsView' and populated cells with dummy data.. Added 'TitleAndBodyView' as reusable 'view'.
    
    * Created 'DataFetcher' class. Populated 'comments' with real data.
    
    * Added `guard` to `error` in `DataFetcher`'s `getData` method.
    
    * Code refactoring.
    
    * Removed unnecessary `viewDidAppear` method from `PostsViewController`.

--- Marsiple/App/Posts/PostsViewController.swift
+++ Marsiple/App/Posts/PostsViewController.swift
@@ -11,16 +11,15 @@ import SnapKit
 class PostsViewController: UIViewController {
     private var posts: [Post] = []
     private let postsView = PostsView.autolayoutView()
-    private let postsFetcher = PostsFetcher()
     
     init() {
         super.init(nibName: nil, bundle: nil)
         setupView()
         setupNavigationBar()
-        postsFetcher.getPosts(success: { [weak self] posts in
+        DataFetcher.getPosts(success: { [weak self] posts in
                                 self?.posts = posts
                                 self?.postsView.tableView.reloadData() },
-                              failure: { error in
+                             failure: { error in
                                 print(error.errorDescription) })
     }
     
@@ -35,9 +34,17 @@ extension PostsViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "PostTableViewCell", for: indexPath) as? PostTableViewCell else { return UITableViewCell() }
-        cell.updateCell(withPost: posts[indexPath.row])
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: CellReuseIdentifier.postTableViewCell, for: indexPath) as? PostTableViewCell else { return UITableViewCell() }
+        let post = posts[indexPath.row]
+        cell.updateCell(title: post.title, body: post.body)
         return cell
+    }
+}
+
+extension PostsViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let postDetailsViewController = PostDetailsViewController(post: posts[indexPath.row])
+        navigationController?.pushViewController(postDetailsViewController, animated: true)
     }
 }
 
@@ -51,6 +58,7 @@ private extension PostsViewController {
         view.backgroundColor = .white
         view.addSubview(postsView)
         postsView.tableView.dataSource = self
+        postsView.tableView.delegate = self
         postsView.snp.makeConstraints {
             $0.edges.equalTo(view.safeAreaLayoutGuide)
         }
